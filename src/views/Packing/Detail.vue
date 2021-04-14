68e0bdfe1e466b57ff13dd35d59c560620767c06
arya <aryayudhareynardo@gmail.com>
2021-04-14 18:05:28 +0700

    Upload Fix

--- src/views/Packing/Detail.vue
+++ src/views/Packing/Detail.vue
@@ -445,10 +445,10 @@
         let data = []
         convertedData.body.forEach((item) => {
           data.push({
-            packing_item_id: item.Packing_Item_Id,
-            total_pack: parseFloat(item.Total_Pack),
-            total_kg: parseFloat(item.Total_Kg),
-            helper_id: item.Packer_Id,
+            'packing_item_id' : item.Packing_Item_Id,
+            'total_pack': parseFloat(item.Total_Pack),
+            'total_kg': parseFloat(item.Total_Kg),
+            'helper_id': item.Packer_Id,
           })
         })
         let send = {
